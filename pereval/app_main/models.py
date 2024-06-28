c2a98b3038fd90c3f52fc8ace55527b8e8586edb
Danil Derkachev <derkachev.danil2002@mail.ru>
2024-06-28 14:50:44 +0300

    Добавил модели

--- pereval/app_main/models.py
+++ pereval/app_main/models.py
@@ -1,7 +1,42 @@
 from django.db import models
 
 
-class Coordinates(models.Model):
-    latitude = models.FloatField(max_length=254)
-    longitude = models.FloatField(max_length=254)
-    height = models.IntegerField()
+class User(models.Model):
+    email = models.EmailField(verbose_name='Электронная почта', unique=True)
+    phone = models.CharField(verbose_name='Номер телефона', max_length=11)
+    surname = models.CharField(verbose_name='Фамилия', max_length=256)
+    name = models.CharField(verbose_name='Имя', max_length=256)
+    patronym = models.CharField(verbose_name='Отчество', max_length=256)
+
+
+class Coords(models.Model):
+    latitude = models.FloatField(verbose_name='Широта', max_length=256)
+    longitude = models.FloatField(verbose_name='Долгота', max_length=256)
+    height = models.IntegerField(verbose_name='Высота над уровнем моря')
+
+
+class Pereval(models.Model):
+    NEW, PENDING, ACCEPTED, REJECTED = 'NE', 'PE', 'AC', 'RE'
+    STATUS_CHOICES = [(NEW, 'new'),
+                      (PENDING, 'pending',),
+                      (ACCEPTED, 'accepted',),
+                      (REJECTED, 'rejected')]
+    status = models.CharField(verbose_name='Статус', max_length=2, choices=STATUS_CHOICES, default=NEW)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    coords_id = models.OneToOneField(Coords, on_delete=models.CASCADE)
+    beautyTitle = models.CharField(max_length=256)
+    title = models.CharField(max_length=256)
+    other_titles = models.CharField(max_length=256)
+    connect = models.TextField(blank=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+    level_spring = models.CharField(verbose_name='Уровень сложности весной', max_length=5, blank=True)
+    level_summer = models.CharField(verbose_name='Уровень сложности летом', max_length=5, blank=True)
+    level_autumn = models.CharField(verbose_name='Уровень сложности осенью', max_length=5, blank=True)
+    level_winter = models.CharField(verbose_name='Уровень сложности зимой', max_length=5, blank=True)
+
+
+class Image(models.Model):
+    pereval_id = models.ForeignKey(Pereval, on_delete=models.CASCADE)
+    image = models.BinaryField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=256, blank=True)
