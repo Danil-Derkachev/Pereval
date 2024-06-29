d50b0172721c5cf7b31fa65658b9df342f7ef573
Danil Derkachev <derkachev.danil2002@mail.ru>
2024-06-29 20:00:34 +0300

    Создал сериалайзер перевала

---
+++ pereval/app_main/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from .models import Pereval
+
+
+class PerevalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pereval
+        fields = [
+            'user',
+            'coords',
+            'beautyTitle',
+            'title',
+            'other_titles',
+            'connect',
+            'level_spring',
+            'level_summer',
+            'level_autumn',
+            'level_winter'
+        ]
