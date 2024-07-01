621b5229a837861d7769dda166af0ad6a718b0ac
Danil Derkachev <derkachev.danil2002@mail.ru>
2024-07-01 20:55:03 +0300

    Добавил url для PerevalListAPIView

--- pereval/app_main/urls.py
+++ pereval/app_main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import PerevalCreateAPIView, PerevalRetrieveAPIView, PerevalRetrieveUpdateAPIView
+from .views import PerevalCreateAPIView, PerevalRetrieveAPIView, PerevalRetrieveUpdateAPIView, PerevalListAPIView
 
 urlpatterns = [
     path('submitData/create', PerevalCreateAPIView.as_view()),
     path('submitData/<int:pk>', PerevalRetrieveAPIView.as_view()),
     path('submitData/<int:pk>/update', PerevalRetrieveUpdateAPIView.as_view()),
+    path('submitData/<str:email>', PerevalListAPIView.as_view()),
 ]
