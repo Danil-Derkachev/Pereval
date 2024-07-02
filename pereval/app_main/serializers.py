8427b4ce02f46ec25773e65380a3f7fbc297e118
Danil Derkachev <derkachev.danil2002@mail.ru>
2024-07-02 21:05:44 +0300

    Переопределил метод create в PerevalCreateSerializer и добавил сериализаторы для пользователя, координат и изображений

--- pereval/app_main/serializers.py
+++ pereval/app_main/serializers.py
@@ -1,23 +1,48 @@
 from rest_framework import serializers
 
-from .models import Pereval
+from .models import Pereval, User, Coords, Image
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'phone', 'surname', 'name', 'patronym']
+
+
+class CoordsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coords
+        fields = ['latitude', 'longitude', 'height']
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['image', 'title']
 
 
 class PerevalCreateSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    coords = CoordsSerializer()
+    images = ImageSerializer(many=True)
+
     class Meta:
         model = Pereval
-        fields = [
-            'user',
-            'coords',
-            'beauty_title',
-            'title',
-            'other_titles',
-            'connect',
-            'level_spring',
-            'level_summer',
-            'level_autumn',
-            'level_winter'
-        ]
+        fields = ['user', 'coords', 'images', 'beauty_title', 'title', 'other_titles', 'connect', 'level_spring',
+                  'level_summer', 'level_autumn', 'level_winter']
+
+    def create(self, validated_data):
+        user_dict = validated_data.pop('user')
+        coords_dict = validated_data.pop('coords')
+        images_list = validated_data.pop('images')
+        user = User.objects.create(**user_dict)
+        coords = Coords.objects.create(**coords_dict)
+        pereval = Pereval.objects.create(**validated_data, user=user, coords=coords)
+        for img in images_list:
+            image = img.pop('image')
+            title = img.pop('title')
+            Image.objects.create(pereval=pereval, image=image, title=title)
+        return pereval
 
 
 class PerevalRetrieveSerializer(serializers.ModelSerializer):
@@ -29,7 +54,7 @@ class PerevalRetrieveSerializer(serializers.ModelSerializer):
 class PerevalRetrieveUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pereval
-        exclude = ['user']
+        exclude = ['id', 'user', 'status']
 
 
 class PerevalListSerializer(serializers.ModelSerializer):
